9d52f58f9a1889787b85d18841e1bc8ef6742b35
Galieva Lilya <lgalieva@baziz.ru>
2019-02-14 13:29:26 +0300

    Задание 3. Добавлены тесты для проверки корректности вычисления дистанции между точками на плоскости

--- sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/DistanceTest.java
@@ -11,8 +11,8 @@ public class DistanceTest {
         Assert.assertEquals(p.distanceSecond(),0.0);
         System.out.println("First test - done!");
 
-        PointSecond p1 = new PointSecond(2,2, 2, 2);
-        Assert.assertEquals(p1.distanceSecond(),0.0);
+        PointSecond p1 = new PointSecond(2,2, 2, 4);
+        Assert.assertEquals(p1.distanceSecond(),1);
         System.out.println("Second test - done!");
 
     }
