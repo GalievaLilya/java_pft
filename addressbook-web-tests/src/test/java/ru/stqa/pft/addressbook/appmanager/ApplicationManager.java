890cefff823aebcf0b612f5b3ce2e1dca7fb25b7
Galieva Lilya <lgalieva@baziz.ru>
2019-02-20 21:35:19 +0300

    Задание 7
    Внесены изменения в ApplicationManager для работы с разными браузерами

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,8 +3,8 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import sun.plugin2.util.BrowserType;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
@@ -13,12 +13,22 @@ public class ApplicationManager {
     private GroupHelper groupHelper;
     private SessionHelper sessionHelper;
     private ContactHelper contactHelper;
+    private String browser;
 
     WebDriver wd;
 
+    public ApplicationManager(String browser){
+        this.browser = browser;
+    }
+
     public void init() {
-       // String browser = BrowserType.FIREFOX;
-        wd = new FirefoxDriver();
+        if (browser.equals(BrowserType.FIREFOX)) {
+            wd = new FirefoxDriver();
+        }else if (browser.equals(BrowserType.CHROME)){
+            wd = new ChromeDriver();
+        }else if (browser.equals(BrowserType.IE)){
+            wd = new InternetExplorerDriver();
+        }
         wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         wd.get("http://localhost:8080/addressbook/");
         groupHelper = new GroupHelper(wd);
