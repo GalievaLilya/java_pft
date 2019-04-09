7e37b67ad47013eff1621808bff115342e904663
cbr <45486416+GalievaLilya@users.noreply.github.com>
2019-04-09 21:03:14 +0300

    Задание 12 Реализация генераторов тестовых данных

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,9 +1,19 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -12,7 +22,23 @@ import static org.testng.Assert.*;
 
 public class GroupCreationTests extends TestBase{
 
-  @Test
+  @DataProvider
+  public Iterator<Object[]> validGroups() throws IOException{
+    //List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+    String xml = "";
+    String line = reader.readLine();
+    while (line != null){
+      xml += line;
+      line = reader.readLine();
+    }
+    XStream xStream = new XStream();
+    xStream.processAnnotations(GroupData.class);
+    List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml);
+    return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
   public void testGroupCreation(){
     app.goTo().groupPage();
     Groups before = app.group().all();
